caf0c52489b08592cd1eeb085874475abd6c4774
Alex A. Naanou <alex.nanou@gmail.com>
2023-09-20 16:37:22 +0300

    ...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ v2/scripts/bootstrap.js
@@ -0,0 +1,64 @@
+/**********************************************************************
+* 
+*
+*
+**********************************************************************/
+
+var fs = require('fs')
+var glob = require('glob')
+
+
+/*********************************************************************/
+
+var bootstrap = {}
+
+var BOOTSTRAP_TEMPLATE = 
+`// This file is generated automatically, all changes made here will be lost.
+
+var Bootstrap = $BOOTSTRAP 
+
+typeof(module) != "undefined"
+	&& (module.exports = Bootstrap)`
+
+
+// XXX add support for json...
+glob('bootstrap/**/*.@(tpl|md|css|html|txt)')
+	.on('match', function(path){
+		var p = path
+			.replace('bootstrap/', '')
+			.replace(/\.(json|txt|md|css|html|txt)/, '')
+		console.log('Found:', p)
+		bootstrap[p] = {
+			text: fs.readFileSync(path).toString(),
+		} })
+	.on('end', function(){
+
+		// extra root stuff...
+		if(fs.existsSync('README.md')){
+			console.log('Setting:', 'About')
+			bootstrap['Doc/About'] = {
+				text: fs.readFileSync('README.md').toString(),
+			} }
+		if(!bootstrap.WikiHome){
+			console.log('Setting:', 'WikiHome')
+			bootstrap.WikiHome = {
+				text: '@include(Doc/About)'
+			} }
+		if(fs.existsSync('LICENSE')){
+			console.log('Setting:', 'LICENSE')
+			bootstrap['LICENSE'] = {
+				text: `${ 
+						fs.readFileSync('LICENSE').toString() 
+					}<!-- @filter(text) -->`,
+			} }
+
+		var txt = BOOTSTRAP_TEMPLATE
+			.replace(/\$BOOTSTRAP/g, JSON.stringify(bootstrap))
+
+		console.log('Writing:', 'bootstrap.js')
+		fs.writeFileSync('bootstrap.js', txt) })
+
+
+
+/**********************************************************************
+* vim:set ts=4 sw=4 :                                                */
